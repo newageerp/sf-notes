db336999b3aed2c3e55ffcf3652bad07619238db
Arturas Jegorenkovas <jegorenkov@gmail.com>
2022-03-19 12:09:23 +0000

    init

---
+++ Object/NoteBase.php
@@ -0,0 +1,120 @@
+<?php
+namespace Newageerp\SfNotes\Object;
+
+use Doctrine\ORM\Mapping as ORM;
+use OpenApi\Annotations as OA;
+
+class NoteBase
+{
+    /**
+     * @OA\Property(format="text", type="string")
+     * @ORM\Column(type="text")
+     */
+    protected string $content = '';
+
+    /**
+     * @OA\Property(title="parentId")
+     * @ORM\Column(type="integer")
+     */
+    protected int $parentId = 0;
+
+    /**
+     * @OA\Property(title="parentSchema")
+     * @ORM\Column(type="string")
+     */
+    protected string $parentSchema = '';
+
+    /**
+     * @ORM\Column (type="json")
+     * @OA\Property (type="array", @OA\Items(type="number|string"))
+     */
+    protected ?array $notify = [];
+
+    /**
+     * @ORM\Column (type="json")
+     * @OA\Property (type="array", @OA\Items(type="number|string"))
+     */
+    protected ?array $notifyAccept = [];
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return int
+     */
+    public function getParentId(): int
+    {
+        return $this->parentId;
+    }
+
+    /**
+     * @param int $parentId
+     */
+    public function setParentId(int $parentId): void
+    {
+        $this->parentId = $parentId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getParentSchema(): string
+    {
+        return $this->parentSchema;
+    }
+
+    /**
+     * @param string $parentSchema
+     */
+    public function setParentSchema(string $parentSchema): void
+    {
+        $this->parentSchema = $parentSchema;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getNotify(): ?array
+    {
+        return $this->notify;
+    }
+
+    /**
+     * @param array|null $notify
+     */
+    public function setNotify(?array $notify): void
+    {
+        $this->notify = $notify;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getNotifyAccept(): ?array
+    {
+        return $this->notifyAccept;
+    }
+
+    /**
+     * @param array|null $notifyAccept
+     */
+    public function setNotifyAccept(?array $notifyAccept): void
+    {
+        $this->notifyAccept = $notifyAccept;
+    }
+
+
+}
